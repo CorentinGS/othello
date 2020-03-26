c46a81fe96dc2278f799354bb3100a434aaf74cc
Yume <43623834+yumenetwork@users.noreply.github.com>
2020-03-26 14:31:53 +0000

    Add rules

--- board.py
+++ board.py
@@ -3,18 +3,18 @@ from termcolor import colored
 
 board_row_top = '-------------------------------'
 
-white_piece = colored('●', 'white')
-white_legal_move = colored('●', 'white', attrs=['blink'])
-black_piece = colored('●', 'blue')
-black_legal_move = colored('●', 'blue', attrs=['blink'])
+white_piece = colored('●', 'blue')
+white_legal_move = colored('●', 'blue', attrs=['blink'])
+black_piece = colored('●', 'white')
+black_legal_move = colored('●', 'white', attrs=['blink'])
 board_piece = {0: '   |', 1: ' ' + white_piece + ' |', 2: ' ' + black_piece + ' |'}
 
 
 # Print Board
 def print_board(board):
     # Scores
-    score_white = len(np.where(board == 1)[0])
-    score_black = len(np.where(board == 2)[0])
+    score_black = len(np.where(board == 1)[0])
+    score_white = len(np.where(board == 2)[0])
     # Top row
     print('  ' + colored('/' + board_row_top + '\\', 'blue'))
     for i in range(0, 8):
@@ -25,9 +25,9 @@ def print_board(board):
         row = row[:-1] + colored('|', 'blue')
         # Afficher les scores
         if i == 2:
-            row += colored('	   Score | ', 'red') + 'White: ' + str(score_white)
+            row += colored('	   Score | ', 'red') + colored('Black: ' + str(score_black), 'blue')
         if i == 3:
-            row += colored('	         | ', 'red') + colored('Black: ' + str(score_black), 'blue')
+            row += colored('	         | ', 'red') + 'White: ' + str(score_white)
         print(row)
         if not i == 7:
             print(colored('  +', 'blue') + board_row_top + colored('+', 'blue'))
